da6987417601cbbf4dd23e92a5b59a634236b253
Kovács Erik <kovi.erik98@gmail.com>
2023-11-12 21:58:43 +0100

    adding addProduct function to the add-product page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,14 +1,35 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export const metadata = {
   title: "Add Product - Flowmazon",
 };
 
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields!");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
 const AddProductPage = () => {
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           required
           name="name"
